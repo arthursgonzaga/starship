91d2fbcc549baa1d7f7c96ef8eba60f05a4ddcd3
Arthur Simões Gonzaga <arthursgonzaga@gmail.com>
2022-07-30 19:24:50 -0300

    first_commit

---
+++ galen/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GalenConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'galen'
---
+++ galen/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+STATUS = ((0, "Draft"), (1, "Publish"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.CharField(max_length=200, default='Arthur')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
